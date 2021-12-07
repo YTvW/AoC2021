56866a9f6054225f04a0da197766c0c3e9d3cfb8
Yrjan van Wijk <yrjan@live.nl>
2021-12-07 15:34:12 +0100

    add part 2

--- day7/day7-part2.py
+++ day7/day7-part2.py
@@ -1,17 +1,29 @@
 import sys
 import fileinput
 import time
-
+from statistics import mean
+from math import ceil, floor
 if len(sys.argv) >=2:
   fileName = sys.argv[1]
 else:
   fileName = "input"
 
+positions =[]
 
-startTime = time.time()
 for line in fileinput.input('./'+fileName+'.txt'):
     cleanLine = line.strip("\n")
-    print(cleanLine)
+    positions=list(map(lambda pos: int(pos),cleanLine.split(',')))
 
+startTime = time.time()
+meanPos=mean(positions)
+avgCommonposMin = floor(meanPos)
+avgCommonposMax =ceil(meanPos)
+
+fuelUsedMin = sum(list(map(lambda pos: sum(range(0,abs(avgCommonposMin-pos)+1)) ,positions)))
+fuelUsedMax = sum(list(map(lambda pos: sum(range(0,abs(avgCommonposMax-pos)+1)) ,positions)))
+if fuelUsedMax > fuelUsedMin:
+  print('result: ',fuelUsedMin)
+else:
+  print('result: ',fuelUsedMax)
 
 print("--- %s seconds ---" % (time.time() - startTime))
