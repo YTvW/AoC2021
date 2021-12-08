da0b43943a7d5a500235ccc4ab589906e108374e
Yrjan van Wijk <yrjan@live.nl>
2021-12-08 15:42:31 +0100

    finish day1

--- day8/day8-part1.py
+++ day8/day8-part1.py
@@ -7,11 +7,27 @@ if len(sys.argv) >=2:
 else:
   fileName = "input"
 
+data = []
+
+def checkLengths(element):
+  length = len(element)
+  if length == 2:
+    return True
+  elif length == 3:
+    return True
+  elif length == 4:
+    return True
+  elif length == 7:
+    return True
+  else:
+    return False
 
 startTime = time.time()
 for line in fileinput.input('./'+fileName+'.txt'):
-    cleanLine = line.strip("\n")
-    print(cleanLine)
-
-
+    cleanLine = line.strip("\n").split(' | ')[1].split()
+    data.extend(cleanLine)
+    # print(cleanLine)
+# print(data)
+lengths = list(filter(lambda element: checkLengths(element) ,data))
+print('result: ',len(lengths))
 print("--- %s seconds ---" % (time.time() - startTime))
