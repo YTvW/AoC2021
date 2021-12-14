2912d27679af99f9b950bc77d7d90ae534b191e4
Yrjan van Wijk <yrjan@live.nl>
2021-12-14 22:21:21 +0100

    finish day14

--- day14/part2.py
+++ day14/part2.py
@@ -3,42 +3,54 @@ import fileinput
 import time
 from copy import deepcopy
 from typing import Counter
+from math import ceil
 if len(sys.argv) >=2:
   fileName = sys.argv[1]
 else:
   fileName = "input"
 
-def updatePolymer(polymer,rules):
-  oldPolymer = deepcopy(polymer)
-  elementsToInsert=[]
-  for i,val in enumerate( oldPolymer):
-    try:
-      pair = val+oldPolymer[i+1]
-    except IndexError:
-      continue
-    elementsToInsert.append((i+1,rules[pair]))
-  elementsToInsert.reverse()
-
-  for element in elementsToInsert:
-    polymerBase.insert(element[0],element[1])
-
-polymerBase =[]
+polymerBase ={}
 pir={}
+startLine=''
 startTime = time.perf_counter()
 for line in fileinput.input('./'+fileName+'.txt'):
     cleanLine = line.strip("\n")
-    if not polymerBase:
-      polymerBase.extend(list(cleanLine))
+    if not startLine:
+      startLine = cleanLine
     elif cleanLine != '':
       pair,sub =cleanLine.split(' -> ')
-      pir[pair]=sub
+      pir[pair]=[(list(pair)[0]+sub),(sub+list(pair)[1])]
+      polymerBase[pair]=0
 
-# print(polymerBase)
-# print(pir)
+polymerTemplate = deepcopy(polymerBase)
+
+for i,val in enumerate(startLine):
+      try:
+        polymerBase[val+startLine[i+1]]+=1
+      except IndexError:
+        break
 
 for steps in range(40):
-  updatePolymer(polymerBase,pir)
-count = Counter(polymerBase)
-common = count.most_common()
-print('result: ',common[0][1]-common[-1][1])
+  tempDict= deepcopy(polymerTemplate)
+  for k,val in polymerBase.items():
+    for pair in pir[k]:
+        # print(pair)
+        tempDict[pair]+= val
+  polymerBase = deepcopy(tempDict)
+
+counts={}
+for k,val in polymerBase.items():
+  char1,char2 = list(k)
+  try:  
+    counts[char1]+=val
+  except:
+    counts[char1]=val
+  try:
+    counts[char2]+=val
+  except:
+    counts[char2]=val
+
+counters =Counter(counts)
+common = counters.most_common()
+print('result: ',ceil(common[0][1]/2)-ceil(common[-1][1]/2))
 print("--- %s seconds ---" % (time.perf_counter() - startTime))
