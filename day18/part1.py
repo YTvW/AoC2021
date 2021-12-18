ea9d968ca7ff191a58c7cb8498d252d7524d501a
YTvW <yrjan@live.nl>
2021-12-18 18:59:20 +0100

    try stuff

--- day18/part1.py
+++ day18/part1.py
@@ -15,11 +15,33 @@ def explode(data,pair):
 def split(data,pair):
 
   pass
+def stringToArray(str):
+  arr=[]
 
+  for i,c in enumerate(str):
+    print(c)
+    if c == '[':
+      stringToArray(str[i::])
+    elif c == ']':
+      return str[::i]
+  pass
+
+
+lastLine=str()
+tempLine=str()
 startTime = time.time()
 for line in fileinput.input('./'+fileName+'.txt'):
     cleanLine = line.strip("\n")
-    print(cleanLine)
+    if len(lastLine)>=1:
+      # input("Press Enter to continue...")
+      tempLine= f'[{lastLine},{cleanLine}]'
+      lastLine=tempLine
+    else:
+      lastLine = cleanLine
+    
+    # print(tempLine)
 
+l = "[1,1]"
+print(stringToArray(l))
 
 print("--- %s seconds ---" % (time.time() - startTime))
