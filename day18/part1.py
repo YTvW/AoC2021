2fad939edeaaf8149b52ee3c735f1d0b35fd1546
Yrjan van Wijk <yrjan@live.nl>
2021-12-19 17:26:09 +0100

    some tests

--- day18/part1.py
+++ day18/part1.py
@@ -1,47 +1,90 @@
 import sys
 import fileinput
 import time
-
+from copy import deepcopy
 if len(sys.argv) >=2:
   fileName = sys.argv[1]
 else:
   fileName = "input"
 
-def explode(data,pair):
-  
+def depth(L): return isinstance(L, list) and max(map(depth, L))+1
 
+def checkExplode(data):
+  level = depth(data)
+  if level >4:
+    return True
+  return False
+
+def levels(l, depth = -1):
+    if not isinstance(l, list):
+        yield (l, depth)
+    else:
+        for sublist in l:
+            yield from levels(sublist, depth + 1)
+
+def reduce(data):
+  
+  
+  pass
+
+
+def explode(data):
+  while checkExplode(data):
+    
+    pass
   pass
 
 def split(data,pair):
 
   pass
-def stringToArray(str):
+
+def stringToArray(str,level=0):
+  # print('level: ',level)
   arr=[]
-
-  for i,c in enumerate(str):
-    print(c)
+  values=[]
+  offset=0
+  
+  while offset<len(str):
+    c = str[offset]
+    offset+=1
     if c == '[':
-      stringToArray(str[i::])
+      used,val =stringToArray(str[offset::],level+1)
+      values.append(val)
+      offset+=used
     elif c == ']':
-      return str[::i]
-  pass
-
+      break
+    elif c.isdigit():
+      values.append(int(c))
+    else:
+      continue
+  arr.extend(values)
+  return offset, arr
 
 lastLine=str()
 tempLine=str()
+lastArr=[]
+data=[]
 startTime = time.time()
 for line in fileinput.input('./'+fileName+'.txt'):
     cleanLine = line.strip("\n")
-    if len(lastLine)>=1:
-      # input("Press Enter to continue...")
-      tempLine= f'[{lastLine},{cleanLine}]'
-      lastLine=tempLine
-    else:
-      lastLine = cleanLine
-    
-    # print(tempLine)
+    array = stringToArray(cleanLine)[1][0]
+    if len(lastArr)>=1:
+      data=[lastArr,array]
+      print('data: ',data)
 
-l = "[1,1]"
-print(stringToArray(l))
+      lastArr=deepcopy(data)
+    else:
+      lastArr = deepcopy(array)
+# print(data)
+
+temp =[[[[[9,8],1],2],3],4]
+print(checkExplode(temp))
+print(list(levels(temp)))
+
+# l = "[7,[[[3,7],[4,3]],[[6,3],[8,8]]]]"
+# print(l)
+# # data = stringToArray(l)[1][0]
+# print(data[1])
+# print(stringToArray(l)[1][0])
 
 print("--- %s seconds ---" % (time.time() - startTime))
