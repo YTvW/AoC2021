d1f1d56292c6499647f74388d70e2bfb58cb8950
Yrjan van Wijk <yrjan@live.nl>
2021-12-21 11:20:16 +0100

    finish part1

--- day21/part1.py
+++ day21/part1.py
@@ -7,11 +7,61 @@ if len(sys.argv) >=2:
 else:
   fileName = "input"
 
-
+p1 = p2 =p1Score = p2Score =0
+diceRoll =1
+diceRolls=0
+board=[10,1,2,3,4,5,6,7,8,9]
+print(board)
 startTime = time.time()
 for line in fileinput.input('./'+fileName+'.txt'):
     cleanLine = line.strip("\n")
+    if p1 ==0:
+      p1= int(cleanLine.split(': ')[1])
+    elif p2 ==0:
+      p2= int(cleanLine.split(': ')[1])
     print(cleanLine)
+print(p1,p2)
 
+for i in range(20):
+  print(board[i%10])
 
+while p1Score < 1000 and p2Score <1000:
+  #do role else break
+  # p1
+  rolled = diceRoll +(diceRoll+1)+(diceRoll+2)
+  print('rolled: ',(p1 +rolled)%10)
+  
+  p1 =  (p1 + rolled)%10
+  p1Score += board[p1]
+  diceRoll += 3
+  diceRolls+=3
+  if p1Score >= 1000:
+    print('p1 won')
+    print('dice: ',diceRoll)
+    print("p1Pos: ",p1)
+    print("p2Pos: ",p2)
+    print("p1Score: ",p1Score)
+    print("p2Score: ",p2Score)
+    print("dice Rolled: ",diceRolls)
+    break
+
+  #p2
+  rolled = diceRoll +(diceRoll+1)+(diceRoll+2)
+  print('rolled: ',(p2+rolled)%10)
+  # p2Score += (p2 + (rolled %10))%10
+  p2 = (p2 + rolled)%10
+  p2Score += board[p2]
+  diceRoll += 3
+  diceRoll = diceRoll%100
+  diceRolls+=3
+
+  print('dice: ',diceRoll)
+  print("p1Pos: ",p1)
+  print("p2Pos: ",p2)
+  print("p1Score: ",p1Score)
+  print("p2Score: ",p2Score)
+  print("dice Rolled: ",diceRolls)
+  # input('press enter to continue ')
+scores = [p1Score,p2Score]
+print('result: ',diceRolls*min(scores))
 print("--- %s seconds ---" % (time.time() - startTime))
